3ff5824fed4c4112019eeb84dbb8edf5d1f9032f
cfv7 <hq@colin.red>
2017-05-16 11:15:36 -0400

    need to complete google and bearer strategy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 
 require('dotenv').config();
-const {DATABASE_URL, PORT} = require('./config');
+const { DATABASE_URL, PORT } = require('./config');
 const path = require('path');
-const {User, QuizItem} = require('./models');
+const { User, QuizItem } = require('./models');
 const express = require('express');
 const passport = require('passport');
 const mongoose = require('mongoose');
@@ -18,96 +18,96 @@ let secret = {
   CLIENT_SECRET: process.env.CLIENT_SECRET
 }
 
-if(process.env.NODE_ENV != 'production') {
+if (process.env.NODE_ENV != 'production') {
   secret = require('./secret');
 }
 
 const app = express();
 
-const database = {
-};
+// const database = {
+// };
 
 app.use(passport.initialize());
 
 passport.use(
-    new GoogleStrategy({
-        clientID:  secret.CLIENT_ID,
-        clientSecret: secret.CLIENT_SECRET,
-        callbackURL: `/api/auth/google/callback`
-    },
+  new GoogleStrategy({
+    clientID: secret.CLIENT_ID,
+    clientSecret: secret.CLIENT_SECRET,
+    callbackURL: `/api/auth/google/callback`
+  },
     (accessToken, refreshToken, profile, cb) => {
-        // Job 1: Set up Mongo/Mongoose, create a User model which store the
-        // google id, and the access token
-        User
-        .find({ googleId: profile.id })
+      // Job 1: Set up Mongo/Mongoose, create a User model which store the
+      // google id, and the access token
+      User
+        .findOneAndUpdate({ googleId: profile.id, accessToken })
         .then((users) => {
-            console.log(users)
-        })    
+          console.log(users)
+        })
         .catch(err => {
-            console.error(err)
+          console.error(err)
         })
-        .then(user => {
-            User    
-            .create({
-                accessToken,
-                googleId: profile.id
-            })
-        })
-        // Job 2: Update this callback to either update or create the user
-        // so it contains the correct access token
-        // const user = database[accessToken] = {
-        //     googleId: profile.id,
-        //     accessToken: accessToken
-        // };
-        return cb(null, null);
+        // .then(user => {
+        //   User
+        //     .create({
+        //       accessToken,
+        //       googleId: profile.id
+        //     })
+        // })
+      // Job 2: Update this callback to either update or create the user
+      // so it contains the correct access token
+      // const user = database[accessToken] = {
+      //     googleId: profile.id,
+      //     accessToken: accessToken
+      // };
+      return cb(null, null);
     })
 );
 
 passport.use(
-    new BearerStrategy(
-        (token, done) => {
-            // Job 3: Update this callback to try to find a user with a
-            // matching access token.  If they exist, let em in, if not,
-            // don't.
-            // User.find({accessToken: token})
-            if (!(token in database)) {
-                return done(null, false);
-            }
-            return done(null, database[token]);
-        }
-    )
+  new BearerStrategy(
+    (token, done) => {
+      // Job 3: Update this callback to try to find a user with a
+      // matching access token.  If they exist, let em in, if not,
+      // don't.
+      User.findOne({accessToken: token})
+      if (!(token in database)) {
+        return done(null, false);
+      }
+      return done(null, database[token]);
+    }
+  )
 );
 
 app.get('/api/auth/google',
-    passport.authenticate('google', {scope: ['profile']}));
+  passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/api/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/',
-        session: false
-    }),
-    (req, res) => {
-        res.cookie('accessToken', req.user.accessToken, {expires: 0});
-        res.redirect('/');
-    }
+  passport.authenticate('google', {
+    failureRedirect: '/',
+    session: false
+  }),
+  (req, res) => {
+    res.cookie('accessToken', req.user.accessToken, { expires: 0 });
+    res.redirect('/');
+  }
 );
 
 app.get('/api/auth/logout', (req, res) => {
-    req.logout();
-    res.clearCookie('accessToken');
-    res.redirect('/');
+  req.logout();
+  res.clearCookie('accessToken');
+  res.redirect('/');
 });
 
 app.get('/api/me',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => res.json({
-        googleId: req.user.googleId
-    })
+  passport.authenticate('bearer', { session: false }),
+  (req, res) => res.json({
+    googleId: req.user.googleId
+  })
 );
 
 app.get('/api/questions',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => res.json(['Question 1', 'Question 2'])
+  passport.authenticate('bearer', { session: false }),
+  (req, res) => res.json(['Question 1', 'Question 2'])
 );
 
 // Serve the built client
@@ -116,12 +116,12 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    const index = path.resolve(__dirname, '../client/build', 'index.html');
-    res.sendFile(index);
+  const index = path.resolve(__dirname, '../client/build', 'index.html');
+  res.sendFile(index);
 });
 
 let server;
-function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(databaseUrl, err => {
       if (err) {
@@ -131,32 +131,32 @@ function runServer(databaseUrl=DATABASE_URL, port=PORT) {
         console.log(`Your app is listening on port ${port}`);
         resolve();
       })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
     });
   });
 }
 
 function closeServer() {
   return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
   });
 }
 
 if (require.main === module) {
-    runServer();
+  runServer();
 }
 
 module.exports = {
-    app, runServer, closeServer
+  app, runServer, closeServer
 };
--- server/models.js
+++ server/models.js
@@ -1,18 +1,9 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   googleId: {type: String, required: true},
-  accessToken: {type: String, required: true},
-  email: {type: String, required: false},
-  password: {type: String, required: false}
+  accessToken: {type: String, required: true}
 });
-userSchema.statics.hashPassword = function(password){
-  return bcrypt.hash(password, 10);
-};
-userSchema.methods.validatePassword = function(password){
-  return bcrypt.compare(input, this.password);
-}
 
 const quizItemSchema = mongoose.Schema({
   index: {type: Number, required: true},
