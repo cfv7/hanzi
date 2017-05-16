6ed236f983a5fb20fb50c5b0d589015087269f8a
cfv7 <hq@colin.red>
2017-05-16 19:03:58 -0400

    added store and actions ----still need to fix reducer

--- client/src/components/question-page.js
+++ client/src/components/question-page.js
@@ -1,41 +1,35 @@
 import React from 'react';
 import * as Cookies from 'js-cookie';
+import {connect} from 'react-redux';
+import {getQuestions} from '../actions'
 
-export default class QuestionPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            questions: []
-        };
-    }
+class QuestionPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: []
+    };
+  }
 
-    componentDidMount() {
-        const accessToken = Cookies.get('accessToken');
-        fetch('/api/questions', {
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            }).then(res => {
-            if (!res.ok) {
-                throw new Error(res.statusText);
-            }
-            return res.json();
-        }).then(questions =>
-            this.setState({
-                questions
-            })
-        );
-    }
+  componentDidMount() {
+    this.props.dispatch(getQuestions())
+  }
 
-    render() {
-        const questions = this.state.questions.map((question, index) =>
-            <li key={index}>{question}</li>
-        );
+  render() {
+    const questions = this.state.questions.map((question, index) =>
+      <li key={index}>{question}</li>
+    );
 
-        return (
-            <ul className="question-list">
-                {questions}
-            </ul>
-        );
-    }
+    return (
+      <ul className="question-list">
+        {questions}
+      </ul>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  questions: state.questions
+})
+
+export default connect(mapStateToProps)(QuestionPage)
