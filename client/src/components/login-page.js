4cf765ec2032d96791f1f8b6b1dd8afd1813d64e
Ramon Reyes <reyesjunk@gmail.com>
2017-06-30 00:08:48 -0400

    styped login page

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import './question-page.css';
 
 export default function LoginPage() {
   return (
     
     <div className="login-container">
-      <img className="logo" alt="MandarinX logo" className="title-image" src={require('../images/mandarinx.png')} />
-      <div className="login-button">
-        <a href={'/api/auth/google'}>Google Login</a>
+      <div className="login-header-wrapper">
+      <div className="login-header-container">
+        {/*<img className="logo" alt="MandarinX logo" className="title-image" src={require('../images/mandarinx2.png')} />*/}
+        <div className="login-logo"> 
+          <p className="login-logo-script"> 汉字 </p>
+       </div>
+       <div className="login-title">  
+          <h1>hànzì</h1>
+       </div>
+      </div>
       </div>
 
       <div className="intro">
-        <p> MandarinX is a simple spaced repetition game for learning Chinese characters. </p> 
-        <p> Log in with your Google account to begin.</p>  
-      </div>
-      
-      <div className="intro-container"></div>
+        <p> 
+          Hànzì is a simple spaced repetition game for learning Chinese characters. 
+        </p>
+        <div className="button-container">
+          <button className="login-button">
+            <a href={'/api/auth/google'}>Log in with Google</a>
+          </button>
+        </div>
+      </div>      
   </div>  
   )
 }
